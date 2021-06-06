9538b80f0aa1fdd20df167c7b6cb21d2553a3f26
sweeneyb <tab126@gmail.com>
2021-06-06 12:28:01 -0400

    moving from html to jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h2 style={ {'textAlign': "center"}}>Company Logo</h2>
+
+      <div className="card center rounded">
+      <div style={{'position': "relative"}}>
+      <img src="/images/canvas-oil-La-Grande-Jatte-Georges-Seurat-1884.jpg" alt="Work" className="rounded" style={{"width": "100%"}} />
+      <div className="bottom-left rounded">
+        <h4>
+          <b className="rounded" >Georges Seurat</b>
+        </h4>
+        <p>A Sunday on La Grande Jatte-1884</p>
+        <a href="https://www.britannica.com/biography/Georges-Seurat/images-videos#/media/1/536352/94613">Source</a>
+      </div>
+      </div>
+
+      <div className="center" style={{"textAlign": "center"}}>
+        <button className="button rounded no" style={{"backgroundColor": "red"}} >
+          <div className="buttonText">Not for me</div>
+        </button>
+        <button className="button rounded yes" style={{"backgroundColor": "green"}}>
+          <div className="buttonText">I like this</div>
+        </button>
+      </div>
+    </div>
+
     </div>
   );
 }
